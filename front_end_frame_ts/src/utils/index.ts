a9947980943305f47498c3cb71f7c17c8c900ba3
chenyuyi.144 <chenyuyi.144@bytedance.com>
2025-05-15 22:06:11 +0800

    完成client模拟

--- front_end_frame_ts/src/utils/index.ts
+++ front_end_frame_ts/src/utils/index.ts
@@ -22,3 +22,12 @@ export function callPerPeriod(
   setTimeout(() => callPerPeriod(time, num, func, callback), time);
 }
 
+export function generateRandomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
