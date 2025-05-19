978012845acae0e297254858d59f1d18d74813f9
betterzys <1398353676@qq.com>
2025-05-19 18:04:51 +0800

    feat: impl limit cache by storage capacity, impl config

--- lru/src/http/mod.rs
+++ lru/src/http/mod.rs
@@ -1,5 +1,6 @@
 use crate::http::router::axum_router;
 use crate::lru::lru_cache::LRUCache;
+use config::Config;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
 use tokio::net::TcpListener;
@@ -13,8 +14,26 @@ struct Tools {
     lru_cache: Arc<RwLock<LRUCache<String, Vec<u8>>>>,
 }
 
-pub async fn axum_serve(port: u64) {
-    let lru_cache: Arc<RwLock<LRUCache<String, Vec<u8>>>> = Arc::new(RwLock::new(LRUCache::new(NonZeroUsize::new(500).unwrap())));
+pub async fn axum_serve(config: Config) {
+    let port = config.get::<u16>("port").unwrap();
+    let cache_mode = config.get::<String>("cache_mode").unwrap();
+    let cache_size = config.get::<usize>("cache_size").unwrap();
+
+    let lru_cache = match cache_mode.as_str() {
+        "item" | "default" => {
+            LRUCache::new(NonZeroUsize::new(cache_size).unwrap())
+        }
+        "capacity" => {
+            LRUCache::storage(NonZeroUsize::new(cache_size).unwrap())
+        }
+        "unlimited" => {
+            LRUCache::unbounded()
+        }
+        _ => {
+            LRUCache::new(NonZeroUsize::new(cache_size).unwrap())
+        }
+    };
+    let lru_cache: Arc<RwLock<LRUCache<String, Vec<u8>>>> = Arc::new(RwLock::new(lru_cache));
 
     let axum_app = axum_router(Tools { lru_cache: lru_cache.clone() });
     let listener = TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
